5b6cc1616913bf0375a7b58d8f8a035eca9e2f8f
suraj dhoundiyal <94472419+surajdhoundiyal1@users.noreply.github.com>
2022-07-18 15:59:22 +0530

    Create Trapping_Rainwater.cpp

---
+++ week2/Linked_List_and_Arrays/Trapping_Rainwater.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+long getTrappedWater(long *arr, int n){
+    // Write your code here.
+      long *left = new long[n];
+
+  long *right = new long[n];
+
+   left[0] = arr[0];
+
+   for(int i=1; i<n; i++){
+
+       left[i] =  max(left[i-1], arr[i]);
+
+   }
+
+   right[n-1] = arr[n-1];
+
+   for(int i=n-2; i>=0; i--){
+
+ right[i] = max(right[i+1], arr[i]);
+
+   }
+
+   long ans=0;
+
+   for(int i=0; i<n; i++){
+
+       ans+=(min(left[i], right[i]) - arr[i]);
+
+   }
+
+   return ans;
+
+
+}
