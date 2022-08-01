5c412c22200b92ba087fb526d184f5d8614db9ac
suraj dhoundiyal <94472419+surajdhoundiyal1@users.noreply.github.com>
2022-08-01 15:54:23 +0530

    Create Clone a graph.cpp

---
+++ week4/Graph/Clone a graph.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+graphNode *dfs(graphNode *n, unordered_map<graphNode *, graphNode *> &mp)
+{
+
+    // the given node is not checked
+
+    mp[n] = new graphNode(n->data);
+
+    // now iterate through its neighbours
+
+    vector<graphNode *> v;
+
+    for (auto i : n->neighbours)
+    {
+        // it is a neighbour which has been checked
+        // checked means earlier it was discovered and it has all its
+        // neighbour information as a vector
+        if (mp.find(i) != mp.end())
+            v.push_back(i);
+
+        // the given neighbour has not been discovered earlier
+        else
+        {
+            v.push_back(dfs(i, mp));
+        }
+    }
+
+    n->neighbours = v;
+    return n;
+}
+
+graphNode *cloneGraph(graphNode *node)
+{
+
+    unordered_map<graphNode *, graphNode *> mp;
+
+    if (!node)
+        return NULL;
+    return dfs(node, mp);
+}
